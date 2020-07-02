b2dada776d1a85119d5da27767c1e8607397a7a6
Lorenzito <Lorenzovarela1000@gmail.com>
2020-07-02 11:56:03 -0600

    Sesion 5

---
+++ Sesion 5/Choque.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choque : MonoBehaviour
+{
+    public AudioSource mordida;
+
+    public int cantPizza;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pizza"))
+        {
+            Debug.Log("Choque con pizza");
+            cantPizza++;
+            mordida.Play();
+        }
+    }
+}
---
+++ Sesion 5/ChoquePizza.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoquePizza : MonoBehaviour
+{
+    public GameObject pizza;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Personaje"))
+        {
+            Debug.Log("Choque Personaje");
+            pizza.SetActive(false);
+        }
+    }
+}
