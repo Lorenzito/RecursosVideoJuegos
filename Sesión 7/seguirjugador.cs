7f397b94f3f64ccf396bd27c746e7e7241decd6e
Lorenzito <Lorenzovarela1000@gmail.com>
2020-07-09 00:54:50 -0600

    Update

---
+++ Sesión 7/seguirjugador.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class seguirjugador : MonoBehaviour {
+    public Transform jugador;
+    NavMeshAgent enemigo;
+    private bool dentro = false;
+
+	// Use this for initialization
+	void Start () {
+        enemigo = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = false;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+        if (!dentro)
+        {
+            enemigo.destination = jugador.position;
+        }
+        if (dentro)
+        {
+            enemigo.destination = this.transform.position;
+        }
+	}
+}
