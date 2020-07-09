e2b3ac84ff78bbc2ccef13f12cab7772ba5c0101
Lorenzito <Lorenzovarela1000@gmail.com>
2020-07-09 01:00:12 -0600

    UPDATE

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Aquí encontrarás todos los recursos necesarios para llevar a cabo el curso de desarrollo de videojuegos en Unity.
 
 #Instalacion de Unity:
-www.facebook.com/100022404935671/videos/729812367775588/              
+www.facebook.com/100022404935671/videos/729812367775588/
+#Instalacion de Visual Studio:
+https://www.facebook.com/100022404935671/videos/730594137697411/
 
 #Sesion 1:
 www.facebook.com/100022404935671/videos/728312387925586/            
@@ -11,4 +13,8 @@ www.facebook.com/100022404935671/videos/728314687925356/
 #Sesion 3:
 www.facebook.com/100022404935671/videos/729082567848568/            
 #Sesion 4:
-www.facebook.com/100022404935671/videos/729818114441680/            
+www.facebook.com/100022404935671/videos/729818114441680/   
+#Sesion 5:
+www.facebook.com/100022404935671/videos/730520537704771/
+#Sesion 6:
+www.facebook.com/100022404935671/videos/734019010688257/
---
+++ Sesión 6/Boton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boton : MonoBehaviour
+{
+
+    //Asigna la variable text del UI
+    public Text txt_objetos;
+    public Text txt_Mensaje;
+    public Image img_vida;
+
+    //Almacena la cantidad de objetos
+    private int cantObjetos = 0;
+    private int cantVida = 4;
+
+    //Suma uno a los objetos
+    public void AgregarObjetos()
+    {
+        cantObjetos++;
+        txt_objetos.text = "Objetos: " + cantObjetos;
+    }
+
+    //Quita vida al personaje
+    public void QuitarVida()
+    {
+        //Reducimos la variable de vida en 1
+        cantVida = cantVida - 1;
+        //Checamos cuanto vale la vida
+        if(cantVida == 3){
+            img_vida.color = Color.yellow;
+        }
+        else if (cantVida == 2){
+            img_vida.color = Color.red;
+        }
+        else if (cantVida == 1){
+            img_vida.color = Color.blue;
+        }
+        else
+        {
+            txt_Mensaje.text = "Has muerto";
+        }
+       
+    }
+
+
+
+
+}
