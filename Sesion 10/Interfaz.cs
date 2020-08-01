0f2f7abfe0aaba47937e839e995f6a05e32d0417
Lorenzito <Lorenzovarela1000@gmail.com>
2020-08-01 14:00:58 -0600

    sesion 10

---
+++ Sesion 10/Interfaz.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Interfaz : MonoBehaviour
+{
+    public GameObject panel;
+
+
+    private void Start()
+    {
+        panel = GameObject.FindGameObjectWithTag("Panel");
+        panel.SetActive(false);
+    }
+    public void IniciarNivel()
+    {
+        SceneManager.LoadScene("Nivel 1");
+
+    }
+
+    public void PantallaReiniciar()
+    {
+        panel.SetActive(true);
+    }
+
+    public void ReiniciarNivel()
+    {
+        SceneManager.LoadScene("Nivel 1");
+    }
+
+}
---
+++ Sesion 10/RecolectarMonedas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RecolectarMonedas : MonoBehaviour
+{
+    public int CantMonedas = 0;
+    public Text txt_CantMonedas;
+    public Interfaz interfaz;
+    public GameObject panel;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Moneda"))
+        {
+            CantMonedas++;
+            txt_CantMonedas.text = CantMonedas + "";
+            collision.enabled = false;
+          //collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Peligro"))
+        {
+             interfaz.PantallaReiniciar();
+            //panel.SetActive(true);
+        }
+    }
+
+
+
+    }
+
